fd620bb9e8faa89d2923376e5b0459f07511f7f0
Иван Сорокин <i.sorokin@bars.group>
2020-08-08 11:58:57 +0700

    Add help information

--- src/ru/destinyman/Main.java
+++ src/ru/destinyman/Main.java
@@ -3,6 +3,14 @@ package ru.destinyman;
 public class Main {
 
     public static void main(String[] args) {
-
+        if (args.length == 0){
+            System.out.println("USAGE: \n " +
+                    "-q, --queries-only - generate only gql-queries\n" +
+                    "-h, --help - get current usage info\n" +
+                    "-a, --all - generate graphql, protofiles and gql-queries\n" +
+                    "-qn, --no-query - generate graphql, protofiles WITHOUT gql-queries"
+            );
+            System.exit(0);
+        }
     }
 }
